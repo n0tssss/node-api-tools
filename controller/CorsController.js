fb079092815c78bdb111ae4976b4e6477f32ebd9
N0ts <mail@n0ts.cn>
2022-10-09 10:15:24 +0800

    修复 cors timeout 失效问题

--- controller/CorsController.js
+++ controller/CorsController.js
@@ -6,13 +6,14 @@ module.exports = (router) => {
      * 代跨域
      */
     router.post("/cors", async (request, response) => {
-        const { method, url, params, data, timeout, headers } = request.body;
+        const { method, url, params, data, headers } = request.body;
         if (!method) {
             return response.send(ResponseStatus.FAIL("method 请求方法不能为空！"));
         }
         if (!url) {
             return response.send(ResponseStatus.FAIL("url 请求地址不能为空！"));
         }
+        const timeout = params.timeout || data.timeout;
         const result = await http({
             method,
             url,
