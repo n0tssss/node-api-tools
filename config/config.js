41658e33f4c5869b385b554b20d5e6463b40d6c6
N0ts <mail@n0ts.cn>
2022-10-26 15:47:15 +0800

    补上 config

--- config/config.js
+++ config/config.js
@@ -1,4 +1,10 @@
 module.exports = {
     // 端口号
-    port: 3000
+    port: 3000,
+    // Gitee Access Token
+    accessToken: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    // Gitee API 地址
+    giteeApi: "https://gitee.com/api/v5",
+    // 操作 API Password
+    password: "123456"
 };
